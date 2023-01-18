4cb6bbd16649223c1b26206f15693fa51a502580
vladislav77 <vladperyazev14@gmail.com>
2023-01-18 21:12:35 +0300

    task solved

---
+++ 034/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// Сделайте кнопку, по нажатию на которую в консоль будет
+//  выводится обратный отсчет, начиная с 100.
+let but = document.querySelector('button');
+but.addEventListener('click', function() {
+    let i = 100;
+    let timerId = setInterval(function() {
+        console.log(i--);
+        if (i < 0) {
+            clearInterval(timerId);
+        }
+    }, 1000);
+});
