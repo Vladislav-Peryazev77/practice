dd45cf9e4c2d8e67b770dc7ed7cfca78dc0ea453
vladislav77 <vladperyazev14@gmail.com>
2023-01-27 14:42:07 +0300

    task solved

--- 047/script.js
+++ 047/script.js
@@ -1,13 +1,15 @@
 "use strict";
-// Выведите в консоль число 0. Через секунду выведите число 1,
-//  через две секунды выведите число 2, через 3 секунды выведите число 3. 
-//  И так далее до бесконечности.
-let i = 0;
-function timer() {
-	setTimeout(function() {
-		console.log(++i);
-		
-		timer();
-	}, 1000);
-}
-timer();
+// Дан ol и кнопка. Сделайте так, чтобы по клику на кнопку в конец ol 
+// добавлялся тег li. Сделайте так, чтобы по клику на любой из добавленных 
+// li в конец его текста записывался восклицательный знак.
+let ol = document.querySelector('#elem');
+let button = document.querySelector('#button');
+button.addEventListener('click', function() {
+	let li = document.createElement('li');
+	li.textContent = 'item';
+	ol.appendChild(li);
+	li.addEventListener('click', function() {
+		li.textContent += '!';
+	});
+});
+
