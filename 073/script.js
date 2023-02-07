1603f413daeb583582a2ab016496036203fdb62e
vladislav77 <vladperyazev14@gmail.com>
2023-02-07 22:40:53 +0300

    task solved;

--- 073/script.js
+++ 073/script.js
@@ -6,7 +6,6 @@
 
 // If the input string is empty, return an empty string. The words in the input 
 // String will only contain valid consecutive numbers.
-
 // Examples
 // "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
@@ -30,7 +29,6 @@ function order(words){
   return result.join(' ');
 }
 console.log(order('is2 Thi1s T4est 3a'));
-  
 
 
 
