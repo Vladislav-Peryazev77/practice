492daacbcaa8b1a039de15abf1ec7efd0cfdd966
vladislav77 <vladperyazev14@gmail.com>
2023-02-08 22:37:55 +0300

    task solved

---
+++ 074/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let elems = document.querySelectorAll('ul li');
+for ( let elem of elems) {
+    let remove = document.createElement('a');
+    remove.textContent = ' remove';
+    remove.href = '';
+    elem.appendChild(remove);
+    remove.addEventListener('click', function() {
+        elem.remove();
+        event.preventDefault();
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
