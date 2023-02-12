fda76a4b6700a65b026ab1e9805f7e8d21b33d5b
vladislav77 <vladperyazev14@gmail.com>
2023-02-12 23:18:46 +0300

    task solved

---
+++ 077/script.js
@@ -0,0 +1,15 @@
+"use strict";
+const goals = [8, 1, 1, 3, 2, -1, 5];
+const mostGoalsMatch = Math.max(...goals);
+const numberOfMostGoalsMatch = goals.findIndex((goal) => {return goal === mostGoalsMatch});
+alert(`Самый результативный матч был под номером ${numberOfMostGoalsMatch + 1} В нем было забито ${mostGoalsMatch} гол(ов).`);
+const allMatchesPlayed = goals.filter((goal) => {return goal > 0});
+const mostLowScoreMatch = Math.min(...allMatchesPlayed);
+const allMostLowScoreMatch = allMatchesPlayed.filter((goal) => {return goal === mostLowScoreMatch});
+const goalsSum = allMatchesPlayed.reduce((acc, goal) => {return acc + goal});
+
+console.log(goalsSum);
+
+
+
+
