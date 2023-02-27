45673603cfc00b5a318c5315c7102a9210866779
vladislav77 <vladperyazev14@gmail.com>
2023-02-27 23:48:11 +0300

    tak solved

---
+++ 102/index.js
@@ -0,0 +1,70 @@
+"use strict";
+function runCode() { 
+  console.log('before promise') 
+  return new Promise((resolve) => { 
+    setTimeout(() => { 
+      console.log('Zero Promise') 
+      resolve() 
+    }, 0) 
+    }) 
+}
+setTimeout(() => { 
+  console.log('Zero') 
+}, 0) 
+runCode().then(() => console.log('Zero Promise Invoked')) 
+console.log('One')
+
+// before promise 
+//one 
+//zero 
+//zero promise
+//zero promise invoked
+
+
+
+
+
+// const getData = (callback) => { 
+//   fetch('https://jsonplaceholder.typicode.com/users/1') 
+//       .then((response) => response.json()) 
+//       .then((user) => { 
+//         console.log('Success'); 
+//         callback(user);
+//      })
+//       .catch((error) => { 
+//         console.log(error); 
+//       }); 
+//     }
+
+// getData(() => { 
+//   console.log('user received'); 
+//   const promise = new Promise((resolve) => {
+//      setTimeout(() => { 
+//       resolve('promise resolved'); 
+//     }, 500);
+//   console.log('in promise'); 
+//   setTimeout(() => { 
+//     console.log('last set timeout'); 
+//     }, 400); 
+//   }); 
+
+//   promise.then((result) => { 
+//     console.log(result); 
+//   }) 
+// }); 
+// console.log('End')
+
+//end 
+// success
+// user received 
+// in promise 
+// last set timeout 
+// promise resolved
+
+
+
+
+
+
+
+
