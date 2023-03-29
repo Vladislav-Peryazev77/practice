06f4537b5b5f57bc8c865455965659b94a51f4c0
vladislav77 <vladperyazev14@gmail.com>
2023-03-29 08:41:59 +0300

    task solved

--- 102/index.js
+++ 102/index.js
@@ -1,73 +1,58 @@
-"use strict";
-function runCode() { 
-  console.log('before promise') 
-  return new Promise((resolve) => { 
-    setTimeout(() => { 
-      console.log('Zero Promise') 
-      resolve() 
-    }, 0) 
-    }) 
+'use strict';
+function runCode() {
+  console.log('before promise');
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Zero Promise');
+      resolve();
+    }, 0);
+  });
 }
-setTimeout(() => { 
-  console.log('Zero') 
-}, 0) 
-runCode().then(() => console.log('Zero Promise Invoked')) 
-console.log('One')
+setTimeout(() => {
+  console.log('Zero');
+}, 0);
+runCode().then(() => console.log('Zero Promise Invoked'));
+console.log('One');
 
-
-
-
-// before promise 
-//one 
-//zero 
+// before promise
+//one
+//zero
 //zero promise
 //zero promise invoked
 
-
-
-
-
-// const getData = (callback) => { 
-//   fetch('https://jsonplaceholder.typicode.com/users/1') 
-//       .then((response) => response.json()) 
-//       .then((user) => { 
-//         console.log('Success'); 
+// const getData = (callback) => {
+//   fetch('https://jsonplaceholder.typicode.com/users/1')
+//       .then((response) => response.json())
+//       .then((user) => {
+//         console.log('Success');
 //         callback(user);
 //      })
-//       .catch((error) => { 
-//         console.log(error); 
-//       }); 
+//       .catch((error) => {
+//         console.log(error);
+//       });
 //     }
 
-// getData(() => { 
-//   console.log('user received'); 
+// getData(() => {
+//   console.log('user received');
 //   const promise = new Promise((resolve) => {
-//      setTimeout(() => { 
-//       resolve('promise resolved'); 
+//      setTimeout(() => {
+//       resolve('promise resolved');
 //     }, 500);
-//   console.log('in promise'); 
-//   setTimeout(() => { 
-//     console.log('last set timeout'); 
-//     }, 400); 
-//   }); 
+//   console.log('in promise');
+//   setTimeout(() => {
+//     console.log('last set timeout');
+//     }, 400);
+//   });
 
-//   promise.then((result) => { 
-//     console.log(result); 
-//   }) 
-// }); 
+//   promise.then((result) => {
+//     console.log(result);
+//   })
+// });
 // console.log('End')
 
-//end 
+//end
 // success
-// user received 
-// in promise 
-// last set timeout 
+// user received
+// in promise
+// last set timeout
 // promise resolved
-
-
-
-
-
-
-
-
